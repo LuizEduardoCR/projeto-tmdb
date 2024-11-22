96fee58d8edd2288d3941068523a5a8e214fc736
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2024-11-22 15:18:00 -0300

    começo tutorial

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
-    Authorization: `Bearer COLOQUE AQUI A CHAVE DE LEITURA DA API`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmY5MWNiMzFmNGM1ZWZlZDA4OGY4NjBlMjI0NWVmMCIsIm5iZiI6MTczMjI5Njc0OS43NTcwNzIsInN1YiI6IjY3MzI1ZWI5YTdmZGM4NjA4Nzg4YmM2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YAeLBke8K_iFLWwpAr1Dwja5icg1slaI_kKrsyh1Oic`,
   },
 });
 
